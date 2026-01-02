bfb61d3ddc42cb43605a1f05b38a8aa76b7a0303
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2026-01-02 16:13:06 +0900

    feat: backend base structure and infrastructure implementation

---
+++ backend/src/test/java/io/forklore/global/common/TestEntity.java
@@ -0,0 +1,23 @@
+package io.forklore.global.common;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TestEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public TestEntity(String name) {
+        this.name = name;
+    }
+}

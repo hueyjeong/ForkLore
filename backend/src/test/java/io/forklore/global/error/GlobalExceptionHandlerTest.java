1129be77536835522d267bebe2456b5bb35cfaeb
HueyJeong <wjdgnltkd123@gmail.com>
2026-01-02 07:53:26 +0000

    feat: implement user domain (entity, role, repository) #10 #11 #12

--- backend/src/test/java/io/forklore/global/error/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/io/forklore/global/error/GlobalExceptionHandlerTest.java
@@ -2,7 +2,6 @@ package io.forklore.global.error;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
